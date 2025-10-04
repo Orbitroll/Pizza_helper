cdb9ab58fadd19917d2d9c392cdaa304b68f19ad
Orbitroll <orbitroll@gmail.com>
2025-10-04 20:12:23 +0300

    chenge folder name

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from weather.weather import weather_bp
-from flour.flour import flour_bp
+from ingridiens.flour import flour_bp
 
 app = Flask('pizza_daw_maker')
 app.register_blueprint(weather_bp, url_prefix='/weather')
@@ -8,5 +8,3 @@ app.register_blueprint(flour_bp, url_prefix='/flour')
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
-
-# ...existing code...
