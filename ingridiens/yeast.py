a7e15353f21765229e1b0435d46bdbb7400cb3a9
Orbitroll <orbitroll@gmail.com>
2025-10-04 20:24:22 +0300

    added yesat

--- ingridiens/yeast.py
+++ ingridiens/yeast.py
@@ -6,6 +6,6 @@ yeasts = [("saf-instant", "Instant Yeast"),
           ("fleichmanns", "Active Dry Yeast")
          ]
 
-@yeast_bp.route('/', methods=['GET'])
+@yeast_bp.route('/', methods=['GET'] )
 def get_yeasts():
     return jsonify([{"name": name, "type": type_} for name, type_ in yeasts]), 200
