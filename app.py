7a6e3aea9130e0c87a9afb725013182799d044f7
Orbitroll <orbitroll@gmail.com>
2025-10-09 08:49:55 +0300

    add BP douge

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask('pizza_daw_maker')
 app.register_blueprint(weather_bp, url_prefix='/weather')
 app.register_blueprint(flour_bp, url_prefix='/flour')
 app.register_blueprint(yeast_bp, url_prefix='/yeast')
+app.register_blueprint(dough_bp, url_prefix='/dough')
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
